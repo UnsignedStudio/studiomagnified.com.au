ccec94200539fcd21de62c9d53b9e25c9b99601d
Callum Neideck <cal_neideck@hotmail.com>
2017-10-18 12:36:06 +1100

    MOD

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,11 +16,31 @@ function resizeLogo() {
     document.getElementById('title').style.transform = dY;
 }
 
+function checkEnterArrowPosition() {
+    var containerHeight = document.getElementById('container').clientHeight;
+    console.log(containerHeight);
+    var gap = (document.body.clientHeight - containerHeight) * 0.5;
+    console.log(gap);
+    if (gap < 134) {
+        var bottom = gap - 55;
+        document.getElementById('enter').style.bottom = bottom + 'px';
+    }
+}
+
 var scene = new ScrollMagic.Scene({triggerElement: '', duration: 800})
-scene.setTween('#title', { top: '110%',  transform: 'translateY(0)', 'max-width': '250px' });
+scene.setTween('#title', { top: '110%',  transform: 'translateY(0)' });
 scene.addTo(ctrl);
 
-resizeLogo();
+var scene2 = new ScrollMagic.Scene({triggerElement: '', duration: 800})
+scene2.setTween('#title img', { 'max-width': '250px' });
+scene2.addTo(ctrl);
+
 window.addEventListener('resize', function() {
     resizeLogo();
+    checkEnterArrowPosition();
+});
+
+window.addEventListener('load', function() {
+    resizeLogo();
+    checkEnterArrowPosition();
 });
